4dd972c9c678443dd8173ca04bcf7c8a633c37f6
Xupransh <supranshmurtyofficial@gmail.com>
2017-06-19 13:09:32 +0530

    first commit

---
+++ WatchGame Extension/menuIC.swift
@@ -0,0 +1,9 @@
+//
+//  menuIC.swift
+//  WatchSprite
+//
+//  Created by supransh on 4/3/17.
+//  Copyright © 2017 jayanti supransh murty. All rights reserved.
+//
+
+import Foundation
---
+++ WatchGame Extension/menuInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  menuInterfaceController.swift
+//  WatchSprite
+//
+//  Created by supransh on 4/3/17.
+//  Copyright © 2017 jayanti supransh murty. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class menuInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
